435697711c6a9807afeb8f2851fcdd264435820a
Simone-Corso <scorso26@gmail.com>
2024-01-09 10:37:31 +0100

    ho messo tutto la parte della macrostruttura

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -15,6 +15,32 @@ export default {
     
 }
 </script>
-<style lang="">
+<style scoped>
+
+.footer-business{
+display: flex;
+justify-content: end;
+}
+
+.container-footer-business{
+height: 700px;
+width: 90%;
+border: 1px solid red;
+display: flex;
+justify-content: center;
+align-items: center;
+}
+
+.container-grow{
+height: 80%;
+width: 40%;
+border: 1px solid black;
+}
+
+.container-contatt{
+    height: 80%;
+width: 60%;
+border: 1px solid black;
+}
     
 </style>
