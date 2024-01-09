3382b1cd32ae0ade2cdd63595691b997aefab523
Simone-Corso <scorso26@gmail.com>
2024-01-09 17:48:44 +0100

    ho aggiunto in modo corretto il componente Brand

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,7 @@
 <template>
     <div>
         <CategoryComponent/>
-    </div>
-    <div class="main-brand">
-    <div class="container-brand">
-        <div class="container-title-brand">
-
-        </div>
-        <div class="container-different-brand">
-
-        </div>
-    </div>
+        <BrandComponent/>
     </div>
     <div class="main-marketing">
     <div class="container-marketing-consultant">
@@ -98,40 +89,16 @@
 </template>
 <script>
 import CategoryComponent from './CategoryComponent.vue';
+import BrandComponent from './BrandComponent.vue';
 
 export default {
-    components: { CategoryComponent }
+    components: {CategoryComponent, 
+                BrandComponent,
+                }
+
 }
 </script>
 <style scoped>
-    .main-brand{
-        display: flex;
-        justify-content: center;
-        align-content: center;
-    }
-
-    .container-brand{
-        height: 350px;
-        width: 80%;
-        border: 2px solid black;
-        display: flex;
-        align-content: center;
-        flex-direction: column;
-        
-    }
-    .container-title-brand{
-        height: 150px;
-        width: 100%;
-        border: 2px solid black;
-
-    }
-    .container-different-brand{
-        height: 200px;
-        width: 100%;
-        border: 2px solid black;
-
-    }
-
     .main-marketing{
         display: flex;
         justify-content: center;
--- src/components/BrandComponent.vue
+++ src/components/BrandComponent.vue
@@ -1,11 +1,46 @@
 <template>
-    
+    <div class="main-brand">
+    <div class="container-brand">
+        <div class="container-title-brand">
+
+        </div>
+        <div class="container-different-brand">
+
+        </div>
+    </div>
+    </div>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style>
-    
+<style scoped>
+    .main-brand{
+        display: flex;
+        justify-content: center;
+        align-content: center;
+    }
+
+    .container-brand{
+        height: 350px;
+        width: 80%;
+        border: 2px solid black;
+        display: flex;
+        align-content: center;
+        flex-direction: column;
+        
+    }
+    .container-title-brand{
+        height: 150px;
+        width: 100%;
+        border: 2px solid black;
+
+    }
+    .container-different-brand{
+        height: 200px;
+        width: 100%;
+        border: 2px solid black;
+
+    }
 </style>
