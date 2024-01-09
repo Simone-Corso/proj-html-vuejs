2458ae72a02290fc42a75dada6be0396cd619fb6
Simone-Corso <scorso26@gmail.com>
2024-01-09 17:37:57 +0100

    ho aggiunto un componente per category

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,33 +1,6 @@
 <template>
-    <!--realizzerò con dei container ripetute più volte, perchè sono uguali come metodo.
-    in futuro dividerò in diversi componenti-->
-    <div class="main-category">
-        <div class="container-business-growth">
-            <div class="container-image-business">
-                <img src="/image/image-business.png" alt="Business Image">
-
-            </div>
-            <div class="container-descrzione-brand">
-
-            </div>
-        </div>
-        <div class="container-investors-people">
-            <div class="container-descrizione-house">
-
-            </div>
-            <div class="container-image-house">
-                <img src="/image/image-investor.png" alt="Business Image">
-            </div>
-        </div>
-        <div class="container-social-media-analysis">
-            <div class="container-image-social">
-                <img src="/image/image-house.png" alt="Business Image">
-
-            </div>
-            <div class="container-descrizione-social-proof">
-
-            </div>
-        </div>
+    <div>
+        <CategoryComponent/>
     </div>
     <div class="main-brand">
     <div class="container-brand">
@@ -124,77 +97,13 @@
 </div>
 </template>
 <script>
+import CategoryComponent from './CategoryComponent.vue';
+
 export default {
-    
+    components: { CategoryComponent }
 }
 </script>
 <style scoped>
-
-    .main-category{
-        display: flex;
-        justify-content: center;
-        align-content: center;
-        flex-direction: column;
-        flex-wrap: wrap;
-    }
-    .container-business-growth{
-        height: 750px;
-        width: 80%;
-        border: 2px solid blue;
-        display: flex;
-
-    }
-    .container-image-business{
-        height: 100%;
-        width: 50%;
-        border: 2px solid blue;
-
-    }
-    .container-descrzione-brand{
-        height: 100%;
-        width: 50%;
-        border: 2px solid blue;
-
-    }
-
-    .container-investors-people{
-        height: 750px;
-        border: 2px solid yellow;
-        display: flex;
-
-    }
-    .container-descrizione-house{
-        height: 100%;
-        width: 50%;
-        border: 2px solid yellow;
-
-    }
-    .container-image-house{
-        height: 100%;
-        width: 50%;
-        border: 2px solid yellow;
-
-    }
-
-    .container-social-media-analysis{
-        height: 750px;
-        border: 2px solid green;
-        display: flex;
-
-    }
-    .container-image-social{
-        height: 100%;
-        width: 50%;
-        border: 2px solid green;
-
-    }
-    .container-descrizione-social-proof{
-        height: 100%;
-        width: 50%;
-        border: 2px solid green;
-
-    }
-
     .main-brand{
         display: flex;
         justify-content: center;
--- src/components/CategoryComponent.vue
+++ src/components/CategoryComponent.vue
@@ -1,6 +1,31 @@
 <template>
-    <div>
-        
+    <div class="main-category">
+        <div class="container-business-growth">
+            <div class="container-image-business">
+                <img src="/image/image-business.png" alt="Business Image">
+
+            </div>
+            <div class="container-descrzione-brand">
+
+            </div>
+        </div>
+        <div class="container-investors-people">
+            <div class="container-descrizione-house">
+
+            </div>
+            <div class="container-image-house">
+                <img src="/image/image-investor.png" alt="Business Image">
+            </div>
+        </div>
+        <div class="container-social-media-analysis">
+            <div class="container-image-social">
+                <img src="/image/image-house.png" alt="Business Image">
+
+            </div>
+            <div class="container-descrizione-social-proof">
+
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -8,6 +33,69 @@ export default {
     
 }
 </script>
-<style>
-    
+<style scoped>
+    .main-category{
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        flex-direction: column;
+        flex-wrap: wrap;
+    }
+    .container-business-growth{
+        height: 750px;
+        width: 80%;
+        border: 2px solid blue;
+        display: flex;
+
+    }
+    .container-image-business{
+        height: 100%;
+        width: 50%;
+        border: 2px solid blue;
+
+    }
+    .container-descrzione-brand{
+        height: 100%;
+        width: 50%;
+        border: 2px solid blue;
+
+    }
+
+    .container-investors-people{
+        height: 750px;
+        border: 2px solid yellow;
+        display: flex;
+
+    }
+    .container-descrizione-house{
+        height: 100%;
+        width: 50%;
+        border: 2px solid yellow;
+
+    }
+    .container-image-house{
+        height: 100%;
+        width: 50%;
+        border: 2px solid yellow;
+
+    }
+
+    .container-social-media-analysis{
+        height: 750px;
+        border: 2px solid green;
+        display: flex;
+
+    }
+    .container-image-social{
+        height: 100%;
+        width: 50%;
+        border: 2px solid green;
+
+    }
+    .container-descrizione-social-proof{
+        height: 100%;
+        width: 50%;
+        border: 2px solid green;
+
+    }
 </style>
