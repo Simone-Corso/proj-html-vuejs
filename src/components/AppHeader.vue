4415abb24c2c4819c20178ec37615b3ba06cd405
Simone-Corso <scorso26@gmail.com>
2024-01-09 16:23:42 +0100

    inserito bg intro marketing con posizione relative

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,83 +1,107 @@
 <template>
     <header>
-        <!--realizzerò la macrostruttura del navigation dell'header con un border solid 2px-->
-        <div class="header-menu">
+      <!--realizzerò la macrostruttura del navigation dell'header con un border solid 2px-->
+      <div class="header-menu">
         <div class="navigation-avada-consultant">
-            <NavigationComponent logo="/image/avada-marketing-logo.png"/>
+          <div class="container-consultant">
+            <NavigationComponent logo="/image/avada-marketing-logo.png" />
             <div class="menu-list">
-            <ul>
-          <li v-for="menuItem in navigationData.menuLista" :key="menuItem">{{ menuItem }}</li>
-        </ul>
-    </div>
-        </div>
-    </div>
-
-        <!--realizzerò la parte del container grande, all'interno con due container unlock e Richard-->
-            <div class="container-bg-image">
-                <div class="container-unlock ">
-
-                </div>
-                <div class="container-Richard">
-
-                </div>
-
+              <div class="container-menu">
+                <ul class="menu-items">
+                  <li v-for="menuItem in navigationData.menuLista" :key="menuItem">{{ menuItem }}</li>
+                </ul>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
+  
+      <!--realizzerò la parte del container grande, all'interno con due container unlock e Richard-->
+      <div class="container-bg-image">
+        <img src="/image/marketing-intro.jpg" alt="marketing-intro" class="intro-image">
+        <div class="container-unlock"></div>
+        <div class="container-Richard"></div>
+      </div>
     </header>
-</template>
-<script>
-import NavigationComponent from './NavigationComponent.vue'; 
-
-export default {
+  </template>
+  
+  <script>
+  import NavigationComponent from './NavigationComponent.vue';
+  
+  export default {
     components: {
-        NavigationComponent,
+      NavigationComponent,
     },
     data() {
-        return {
-            navigationData: {
-
-                logo: 'avada-marketing-logo.png',
-                menuLista: ["Home", "Service", "Why Us", "Case Studies", "About", "Blog"],
-                
-            },
-        };
+      return {
+        navigationData: {
+          logo: 'avada-marketing-logo.png',
+          menuLista: ["Home", "Service", "Why Us", "Case Studies", "About", "Blog"],
+        },
+      };
     },
-};
-</script>
-<style scoped>
-    .menu-list{
-        height: 90px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    ul,li{
-        list-style-type: none;
-    }
-    .header-menu{
-        display: flex;
-        justify-content: center;
-    }
-    .navigation-avada-consultant{
-        height: 100px;
-        width: 80%;
-        border: 2px solid red;
-    }
+  };
+  </script>
+  
+  <style scoped>
+  .container-menu {
+    
+    border: 2px solid magenta;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 20px;
+  }
+  
+  .menu-items {
+    list-style-type: none;
+    display: flex;
+    flex-grow: 1; 
+  }
+  
+  .menu-items li {
+    margin-right: 10px; 
+  }
+  
+  .header-menu {
+    display: flex;
+    justify-content: center;
+  }
+  
+  .navigation-avada-consultant {
+    height: 100px;
+    width: 80%;
+    border: 2px solid red;
+    display: flex;
+    justify-content: space-between; 
+  }
+  
+  .container-consultant {
+     
+    border: 2px solid cadetblue;
+    display: flex;
+    align-items: center;
+  }
+  
+  .container-bg-image {
+    height: 600px;
+    border: 2px solid red;
+    display: flex;
+    position: relative;
+  }
 
-    .container-bg-image{
-        height: 800px;
-        border: 2px solid red;
-        display: flex;
-    }
-
-    .container-unlock{
-        width: 50%;
-        height: 100%;
-        border: 2px solid red;
-    }
-
-    .container-Richard{
-        width: 50%;
-        height: 100%;
-        border: 2px solid red;
-    }
-</style>
+  .intro-image{
+    width: 100%
+  }
+  
+.container-unlock,
+.container-Richard {
+  width: 50%;
+  height: 100%;
+  border: 2px solid red;
+  position: absolute; 
+  top: 0;
+}
+  </style>
+  
+  
