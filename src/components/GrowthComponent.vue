31e3dc4f8fe44f12bc985e749829804dbf45bbdd
Simone-Corso <scorso26@gmail.com>
2024-01-10 15:52:38 +0100

    inserisco i loghi per la parte di sotto

--- src/components/GrowthComponent.vue
+++ src/components/GrowthComponent.vue
@@ -1,53 +1,132 @@
 <template>
     <div class="main-work">
-    <div class="container-big">
-    <div class="container-client">
+        <div class="container-big">
+            <div class="container-client">
+               <h1>
+                We Boost Our Client's Bottom Line by Optimizing Their Growth Potential.
+               </h1>
+               <h3>
+                Quisque aliquet, libero consequat alementum convallis, erat risus imperdiet pellentesque sem neque eget.
+               </h3>
+               <button class="btn-work">
+                Why Work With Us
 
-    </div>
-    <div class="container-image">
+               </button>
+            </div>
+            <div class="container-image">
+                <img src="/image/we-boost-our-clients.jpg" alt="boost" class="image-we-boost">
+            </div>
+        </div>
+        <div class="container-big-two">
+            <div class="container-client">
+                <h1 class="title-relationships">
+                    We Build Relationships
+                </h1>
+                <h3 class="descrizione-relationships">
+                    Curabitur ac leo nunc. Vestibulum et mauris vel ante finibus maxiums nec ut leo.
+                    integer consectetur.
+                </h3>
+                <h1 class="title-processes">
+                    Proprietary Processes
+                </h1>
+                <h3 class="descrizione-processes">
+                    Curabitur ac leo nunc. Vestibulum et mauris vel ante finibus maxiums nec ut leo.
+                    integer consectetur.
+                 </h3>
+            </div>
+            <div class="container-image">
+                <article class="container-accountability">
+                    <img src="" alt="">
 
-    </div>
-    <div class="container-awards">
+                </article>
+                <article class="container-trasparency">
+                    <img src="" alt="">
 
+                </article>
+                    <article class="container-investiment">
+                        <img src="" alt="">
+
+                    </article>
+            </div>
+        </div>
+        <div class="container-awards">
+            <!-- Contenuto di container-awards -->
+        </div>
     </div>
-</div>
-</div>
 </template>
-<script>
-export default {
-    
-}
-</script>
-<style>
-     .main-work{
+
+<style scoped>
+    .main-work {
         display: flex;
         justify-content: center;
-
+        flex-direction: column; 
+        align-items: center;
+        margin-top: 5rem;
     }
 
-    .container-big{
-        height: 900px;
+    .container-big {
+        display: flex;
+    justify-content: space-between;
+    height: 500px;
+    width: 80%;
+    border: 2px solid black;
+    margin-bottom: 10px;
+    }
+
+    .container-big-two {
+        display: flex;
+        justify-content: space-between;
+        height: 600px;
         width: 80%;
         border: 2px solid black;
-        display: flex;
-        justify-content: center;
-
+        margin-bottom: 10px; 
     }
 
-    .container-client{
-        height: 450px;
+    .container-client {
+        height: 100%;
+    width: 35%;
+    border: 1px solid red;
+    margin-left: auto;
+    margin-top: 3.5rem;
+    }
+
+    .container-image {
+        height: 100%; 
         width: 50%;
         border: 1px solid red;
-    }
-    .container-image{
-        height: 450px;
-        width: 50%;
-        border: 1px solid red;
+        overflow: hidden;
     }
 
-    .container-awards{
-        height: 450px;
-        width: 100%;
+    .image-we-boost {
+        width: 80%;
+        height: 100%;
+        object-fit: cover; 
+        border-radius: 10px;
+    }
+
+    .title-relationships{
+        font-size: 30px;
+    }
+
+    .descrizione-relationships{
+
+    }
+
+    .title-processes{
+        font-size: 30px;
+    }
+
+    .descrizione-processes{
+        
+    }
+
+    h1{
+        font-size: 50px;
+    }
+
+    .container-awards {
+        height: 300px;
+        width: 80%;
         border: 1px solid green;
     }
 </style>
