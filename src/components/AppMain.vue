e6c2ccb60b00a698c3a3370bfdaabd7d31834afa
Simone-Corso <scorso26@gmail.com>
2024-01-10 10:09:57 +0100

    ho suddiviso in modo corretto la parte del GrowthComponent.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,21 +3,8 @@
         <CategoryComponent/>
         <BrandComponent/>
         <LeadingComponent/>
+        <GrowhComponent />
     </div>
-<div class="main-work">
-    <div class="container-big">
-    <div class="container-client">
-
-    </div>
-    <div class="container-image">
-
-    </div>
-    <div class="container-awards">
-
-    </div>
-</div>
-</div>
-
 <div class="main-team-avada">
     <div class="container-team">
         <div class="container-image-avada">
@@ -64,50 +51,17 @@
 import CategoryComponent from './CategoryComponent.vue';
 import BrandComponent from './BrandComponent.vue';
 import LeadingComponent from './LeadingComponent.vue';
+import GrowhComponent from './GrowhComponent.vue';
 export default {
     components: {CategoryComponent, 
                 BrandComponent,
                 LeadingComponent,
+                GrowhComponent,
                 }
 
 }
 </script>
 <style scoped>
-    
-
-    
-
-    .main-work{
-        display: flex;
-        justify-content: center;
-
-    }
-
-    .container-big{
-        height: 900px;
-        width: 80%;
-        border: 2px solid black;
-        display: flex;
-        justify-content: center;
-
-    }
-
-    .container-client{
-        height: 450px;
-        width: 50%;
-        border: 1px solid red;
-    }
-    .container-image{
-        height: 450px;
-        width: 50%;
-        border: 1px solid red;
-    }
-
-    .container-awards{
-        height: 450px;
-        width: 100%;
-        border: 1px solid green;
-    }
 
         .main-team-avada{
             display: flex;
--- src/components/GrowhComponent.vue
+++ src/components/GrowhComponent.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
-        
+    <div class="main-work">
+    <div class="container-big">
+    <div class="container-client">
+
     </div>
+    <div class="container-image">
+
+    </div>
+    <div class="container-awards">
+
+    </div>
+</div>
+</div>
 </template>
 <script>
 export default {
@@ -9,5 +19,35 @@ export default {
 }
 </script>
 <style>
-    
+     .main-work{
+        display: flex;
+        justify-content: center;
+
+    }
+
+    .container-big{
+        height: 900px;
+        width: 80%;
+        border: 2px solid black;
+        display: flex;
+        justify-content: center;
+
+    }
+
+    .container-client{
+        height: 450px;
+        width: 50%;
+        border: 1px solid red;
+    }
+    .container-image{
+        height: 450px;
+        width: 50%;
+        border: 1px solid red;
+    }
+
+    .container-awards{
+        height: 450px;
+        width: 100%;
+        border: 1px solid green;
+    }
 </style>
