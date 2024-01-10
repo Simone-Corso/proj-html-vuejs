733f21b94ab4fed36d01a0370afe0f1146abef5c
Simone-Corso <scorso26@gmail.com>
2024-01-10 14:43:22 +0100

    inseriti gli immagini del leading

--- src/components/BrandComponent.vue
+++ src/components/BrandComponent.vue
@@ -2,10 +2,10 @@
     <div class="main-brand">
     <div class="container-brand">
         <div class="container-title-brand">
-            <h1>
+            <h1 class="title-brand">
                 Some Brands We Work With
             </h1>
-            <h3>Quisque aliquet, libero consequat elementum convallis.</h3>
+            <h3 class="title-brands">Quisque aliquet, libero consequat elementum convallis.</h3>
         </div>
         <div class="container-different-brand">
 
@@ -34,7 +34,30 @@
     <div class="main-marketing">
     <div class="container-marketing-consultant">
         <div class="container-consultant">
+            <article class="consultant-expert">
             <img src="/image/marketing-expert-cta.png" alt="expert-cta" class="image-expert-cta">
+        </article>
+            <div class="container-title-consultant">
+            <h2>
+                Sandy Reiff
+            </h2>
+            <h3>
+                Marketing Consultant Expert
+            </h3>
+        </div>
+        <div class="container-title-phone">
+            <h1>
+                Drive Leads and Sales with Avada
+            </h1>
+        </div>
+            <div class="container-btn-phone">
+        <button class="btn-phone-consultant">
+            <i class="fa-solid fa-phone"></i> (555) 802-1234
+        </button>
+        </div>
+
+
+            
 
 
 
@@ -74,12 +97,12 @@ export default {
 
     }
 
-    h1{
+    .title-brand{
         font-size: 45px;
         margin-bottom: 0;
     }
 
-    h3{
+    .title-brands{
         font-size: 20px;
         margin-top: .5rem;
         
@@ -105,6 +128,27 @@ export default {
         display: flex;
         justify-content: center;
     }
+
+    h1{
+       font-size: 25px;
+        }
+
+        h3{
+            font-size: 15px;
+        }
+    .consultant-expert{
+
+    }
+
+    .container-title-consultant{
+       margin-top: 0;
+       line-height: 1rem;
+
+    }
+    
+    .container-title-phone{
+        margin-left: 3rem;
+    }
     .container-marketing-consultant{
         height: 100px;
         width: 80%;
@@ -118,12 +162,30 @@ export default {
         width: 80%;
         border: 2px solid magenta;
         border-radius: 10px;
+        display: flex;
+        align-items: center;
     }
 
     .image-expert-cta{
         border-radius: 50%;
-        height: 80px;
+        height: 70px;
         width: auto;
         margin-left: 2.5rem;
+        margin-top: .5rem;
     }
+
+    .container-btn-phone{
+    margin-left: auto;
+    margin-right: 3rem;
+  }
+
+  .btn-phone-consultant{
+    font-size: 15px;
+    height: 50px;
+    width: 200px;
+    border-radius: 10px;
+    border: none;
+    background-color: #FFE7DA;
+    color: #F86011;
+  }
 </style>
--- src/components/LeadingComponent.vue
+++ src/components/LeadingComponent.vue
@@ -2,13 +2,16 @@
    <div class="main-leading">
     <div class="container-leading-organisations">
         <div class="container-first-leading">
-
+            <img src="/image/case-studies-1.jpg" alt="" class="image-leading">
+            <h1>
+                How Spaces attracted five million visitors by improving the content
+            </h1>
         </div>
         <div class="container-two-leading">
-
+            <img src="/image/case-studies-4.jpg" alt="" class="image-leading">
         </div>
 <div class="container-three-leading">
-
+    <img src="/image/case-studies-6.jpg" alt="" class="image-leading">
 </div>
 <div class="container-btn-leading">
         <button class="btn-leading">
@@ -49,6 +52,12 @@ export default {
         height: 80%;
         width: 25%;
         border: 1px solid black;
+    }
+
+    .image-leading{
+        width: 100%;
+        height: 70%;
+        object-fit: cover;
     }
     .container-two-leading{
         height: 80%;
